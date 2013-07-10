5f48de2ad49d25046224c722305b6e77be2d153a
derpston <derpston@sleepygeek.org>
2013-07-10 15:42:00 +0100

    frameserver: Adding the ability to view frames from multiple videos, displayed on the same timeline.

--- frameserver.py
+++ frameserver.py
@@ -7,13 +7,11 @@ import brb
 
 videos = []
 
-buf = brb.BRBReader("/tmp/camera2/", 100 * 1024 * 1024, 10)
-#buf.read(1333326353.8913672)
-start = buf.read(0)[0]
-end = buf.read(2**32)[0]
-print start, end
+buf = brb.BRBReader(sys.argv[1], 100 * 1024 * 1024, 5)
 
-videos.append({'reader': buf, 'start': start, 'end': end})
+for blockid, size, first, last in buf._blocks():
+   # Start and end timestamps.
+   videos.append({'block': blockid, 'start': first[2], 'end': last[2], 'size': size})
 
 urls = (
     '/', 'index',
@@ -31,14 +29,14 @@ class video:
     def GET(self):
         result = []
         for video in videos:
-            result.append({'start': video['start'], 'end': video['end']})
+            result.append({'start': video['start'], 'end': video['end'], 'size': video['size']})
         return json.dumps(result)
 
 class frame:
     def GET(self, video, timestamp_wanted):
         video = int(video)
         timestamp_wanted = float(timestamp_wanted)
-        (timestamp, data) = videos[video]['reader'].read(timestamp_wanted)
+        (timestamp, data) = buf.readblock(video, timestamp_wanted)
         web.header("Content-Type", "image/jpeg")
         web.header("X-Timestamp", timestamp)
         return data
