9b0bd68b410dbcd759d17d4afda56c79022029f9
derpston <derpston@sleepygeek.org>
2013-07-15 23:33:57 +0100

    Adding second timeline, frame searching (to enable later prev/next feature) and various small improvements. One big sloppy commit, oops, got carried away.

--- frameserver.py
+++ frameserver.py
@@ -7,7 +7,7 @@ import brb
 
 videos = []
 
-buf = brb.BRBReader(sys.argv[1], 100 * 1024 * 1024, 5)
+buf = brb.BRBReader("/mnt/omg/camera/", 100 * 1024 * 1024, 0x1b)
 
 for blockid, size, first, last in buf._blocks():
    # Start and end timestamps.
@@ -32,13 +32,38 @@ class video:
             result.append({'start': video['start'], 'end': video['end'], 'size': video['size']})
         return json.dumps(result)
 
+cached_frame = None
+
 class frame:
-    def GET(self, video, timestamp_wanted):
+    def HEAD(self, video, timestamp_wanted):
+        global cached_frame
         video = int(video)
         timestamp_wanted = float(timestamp_wanted)
+
         (timestamp, data) = buf.readblock(video, timestamp_wanted)
+        cached_frame = (video, timestamp, data)
+        url = "/video/%d/%f" % (video, timestamp)
+        web.header("Location", url)
+        
+    def GET(self, video, timestamp_wanted):
+        global cached_frame
+        video = int(video)
+        timestamp_wanted = float(timestamp_wanted)
+
         web.header("Content-Type", "image/jpeg")
-        web.header("X-Timestamp", timestamp)
+
+        try:
+            cached_video, cached_timestamp, cached_data = cached_frame
+            assert cached_video == video
+            assert cached_timestamp == timestamp_wanted
+            data = cached_data
+        except (TypeError, AssertionError), ex:
+            (timestamp, data) = buf.readblock(video, timestamp_wanted)
+
+            if timestamp_wanted != timestamp:
+                web.ctx.status = "404 Not Found"
+                return "404 Not Found"
+
         return data
 
 if __name__ == "__main__":
